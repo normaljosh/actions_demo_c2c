5dfaa5e4f7cfb86913d2d6c145600581ab3738cd
normaljosh <normaljosh@gmail.com>
2022-01-19 23:16:31 +0000

    Initial commit

---
+++ test.py
@@ -0,0 +1,21 @@
+import unittest
+from main import is_odd
+
+class TestIsOdd(unittest.TestCase):
+
+    def test_odd_num(self):
+        self.assertTrue(is_odd(3))
+
+class TestIsEven(unittest.TestCase):
+
+    def test_even_num(self):
+        self.assertFalse(is_odd(2))
+
+class TestIsZero(unittest.TestCase):
+
+    def test_zero(self):
+        self.assertFalse(is_odd(0))
+        
+
+if __name__ == '__main__':
+    unittest.main()
